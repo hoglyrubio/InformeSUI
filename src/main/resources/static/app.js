2d9efdb4a51121ff9e1a1e297f9c77624a99e461
Hogly Leonardo Rubio Tarazona <Hogly Leonardo Rubio Tarazona>
2016-05-09 20:46:55 -0500

    Implemented Periodos front-end side

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -22,13 +22,50 @@ app.get("/informe/periodos", function (req, res) {
 });
 
 app.get("/informe/anios", function (req, res) {
+  var _response = [];
+  console.log(req.param);
+  if (req.query.gt != undefined) {
+    _response = [
+      { "periano": 2015 },
+      { "periano": 2014 },
+      { "periano": 2013 },
+      { "periano": 2012 },
+      { "periano": 2011 }
+    ];        
+  } else if (req.query.lt != undefined) {
+    _response = [
+      { "periano": 2004 },
+      { "periano": 2003 },
+      { "periano": 2002 },
+      { "periano": 2001 },
+      { "periano": 2000 }
+    ];          
+  } else {
+    _response = [
+      { "periano": 2010 },
+      { "periano": 2009 },
+      { "periano": 2008 },
+      { "periano": 2007 },
+      { "periano": 2005 }
+    ];    
+  }
+  res.status(200).send(_response);
+});
+
+app.get("/informe/periodos/:periano", function (req, res) {
   var _response = [
-    { "periano": 2015 },
-    { "periano": 2014 },
-    { "periano": 2013 },
-    { "periano": 2012 },
-    { "periano": 2010 },
-    { "periano": 2009 }
+    { "pericodi": req.params.periano + "12", "periano": req.params.periano, "perimes": 12, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "11", "periano": req.params.periano, "perimes": 11, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "10", "periano": req.params.periano, "perimes": 10, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "09", "periano": req.params.periano, "perimes": 09, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "08", "periano": req.params.periano, "perimes": 08, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "07", "periano": req.params.periano, "perimes": 07, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "06", "periano": req.params.periano, "perimes": 06, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "05", "periano": req.params.periano, "perimes": 05, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "04", "periano": req.params.periano, "perimes": 04, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "03", "periano": req.params.periano, "perimes": 03, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "02", "periano": req.params.periano, "perimes": 02, "perifein": 0, "perifefi": 0 },
+    { "pericodi": req.params.periano + "01", "periano": req.params.periano, "perimes": 01, "perifein": 0, "perifefi": 0 }
   ];
   res.status(200).send(_response);
 });
--- src/main/resources/static/src/app/periodo/periodo.js
+++ src/main/resources/static/src/app/periodo/periodo.js
@@ -7,7 +7,52 @@ new Vue({
     anios: []
   },
 
+  methods: {
+
+    requestPeriodos: function (url) {
+      console.log("periodos: " + url);
+      var self = this;
+      $.get(url, function (data) {
+        self.periodos = data;
+      }).fail(function (error) {
+        var msg = "Error obteniendo Periodos desde:" + url + ". Message:" + error.responseText;
+        console.log(msg);
+        alert(msg);
+      });            
+    },
+    
+    requestAnios: function (url) {
+      console.log("anios: " + url);
+      var self = this;
+      $.get(url, function (data) {
+        self.anios = data;
+      }).fail(function (error) {
+        var msg = "Error obteniendo Periodos desde:" + url + ". Message:" + error.responseText;
+        console.log(msg);
+        alert(msg);
+      });            
+    },
+
+    obtainPeriodosByAnio: function (anio) {
+      this.requestPeriodos("informe/periodos/" + anio);
+    },
+
+    obtainPreviousGroupOfAnios: function (anios) {
+      var first = anios[0].periano; 
+      this.requestAnios("informe/anios?gt=" + first);
+    },
+
+    obtainNextGroupOfAnios: function (anios) {
+      var last = anios[ anios.length - 1 ].periano;
+      this.requestAnios("informe/anios?lt=" + last);
+    }
+  },
+  
   ready: function () {
+    this.requestPeriodos("informe/periodos");
+    this.requestAnios("informe/anios");
+
+/*    
     var self = this;
 
     $.get("informe/periodos", function (data) {
@@ -23,6 +68,6 @@ new Vue({
       console.log(error);
       alert(error.responseText);
     });
-
+*/
   }
 });
